6c189c2b67936781214fc523e375159539a56e2f
liguangyi <liguangyi08@gmail.com>
2024-06-24 09:13:35 +0800

    find a way to overwirte method in test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import util
+# import util
+from util import rename
 
 def use_rename():
-    after_rename = util.rename("name")
+    # after_rename = util.rename("name")
+    after_rename = rename("name")
     return after_rename
 
 print(use_rename())
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,17 @@
 import sys
 import os
 from unittest.mock import patch
+import main
 from main import use_rename
 
-@patch("main.util")
-def test_use_rename(mock_util):
-    mock_util.rename.return_value = "name"
+# @patch("main.rename")
+# def test_use_rename(mocked_rename):
+#     mocked_rename.return_value = "name"
+#     assert use_rename() == "name"
+
+def fake_rename(name: str) -> str:
+    return 'Hello World'
+
+def test_use_rename():
+    main.rename = fake_rename
     assert use_rename() == "name"
